eb86e349e067de9c1544fff9f28328a253e4d31a
DANIELA LINO DA SILVA OLIVEIRA <22282183@dsn127m1246729.cfp127aluno.local>
2023-09-21 16:52:35 -0300

    oitavo commit - organizando os arquivos css, como o reset.css por exemplo.

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Input } from 'antd';
 import "../input/input.css";
-{/* <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
-</style> */}
 
 const InputComponent = ({textFielName} ) =>{
   return (
