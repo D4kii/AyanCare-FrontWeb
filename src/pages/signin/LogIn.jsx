707bdf6522b30178a52972cb7b607e3dc62ab33a
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-09-24 01:43:20 -0300

    estilizando a pagina de login

--- src/pages/signin/LogIn.jsx
+++ src/pages/signin/LogIn.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import '../signin/login.css';
 import Forms from "../../components/forms/forms";
 
 
@@ -8,9 +8,9 @@ function SignIn() {
     console.log(Forms);
 
     return (
-        <div className="LoginScreen">
+        <div className="login-screen">
             <h1>Bem vindo de Volta!</h1>
-            <h3>Transforme o cuidado em uma jornada gratificante.</h3>
+            <h3>AyanCare é tudo que você precisa para administrar, de forma eficiente e prática, sua rotina com seus pacientes e seus compromissos do cotidiano.</h3>
             <Forms/>
         </div>
     )
