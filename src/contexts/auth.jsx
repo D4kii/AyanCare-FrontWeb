e6e0e3c2b1ca9c2ff7ddc355753b6fbf5f0299aa
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-10-13 00:17:05 -0300

    autenticação de login feita, faltando somente a de cadastro

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -11,42 +11,45 @@ export const AuthProvider = ({ children }) => {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-
+    
     useEffect(() => {
         //usuário existente ou não no localStorage
         const recoveredUser = localStorage.getItem("user");
-
+        
         if (recoveredUser) {
             setUser(JSON.parse(recoveredUser));
         }
-
+        
         setLoading(false);
     }, []);
-
-    const login = async (email, password) => {
+    
+    const login = async (email, senha) => {
         
-        const response = await createSessionUsuarioAutenticar(email, password);
+        const response = await createSessionUsuarioAutenticar(email, senha);
+        console.log(response);
         
-        console.log('login auth', response.data);
-
         const loggedUser = response.data.cuidador;
+        const token = response.data.token;
 
         //localStorage só aceita strings e não objetos
         // JSON.stringfy() para fazer essa conversão, já que loggedUser é um objeto
-        localStorage.setItem("user", JSON.stringify(loggedUser));
+        localStorage.setItem("cuidador", JSON.stringify(loggedUser));
+        localStorage.setItem("token", token);
 
-        if (password === 'secret') {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
 
-            setUser(loggedUser);
-            navigate("/home")
-        }
+
+        setUser(loggedUser);
+        navigate("/home")
+
     };
 
     const logout = () => {
         console.log('logout');
-        
-        localStorage.removeItem("user"); //Limpa o localStorage para que a pessoa não tenha mais acesso 
 
+        localStorage.removeItem("cuidador"); //Limpa o localStorage para que a pessoa não tenha mais acesso 
+        localStorage.removeItem("token"); //Limpa o localStorage para que a pessoa não tenha mais acesso 
+        api.defaults.headers.Authorization = null;
         setUser(null);
         navigate("/login")
     };
--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://ayancare-api.cyclic.cloud"
+        baseURL: "https://ayancare-api.cyclic.cloud/v1/ayan",
 });
 
-export const createSessionUsuarioAutenticar = async (email, password) => {
-    return api.post("/usuario/autenticar", { email, password })
+export const createSessionUsuarioAutenticar = async (email, senha) => {
+
+    console.log( { email, senha });
+    return await api.post("/usuario/autenticar", { email, senha }) 
 
 }
+
+// bomdia40028922
