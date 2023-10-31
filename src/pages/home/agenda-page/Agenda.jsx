3ca0a3ced0dc34228ebcd53cde3947ad1cb1784a
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-10-31 03:53:04 -0300

    cards de turnos parcialmente finalizados, começando os componentes de eventos e alarmes

--- src/pages/home/agenda-page/Agenda.jsx
+++ src/pages/home/agenda-page/Agenda.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 //Componentes
 import Menu from '../../../components/menu/menu';
 import CalendarComponent from '../../../components/calendario/Calendar';
 import CardTurno from '../../../components/card-turno/CardTurno';
+import { Carousel, Radio } from 'antd';
 
 import './agenda.css'
 
-const Agenda = () => {
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 
+const Agenda = () => {
+    const [dotPosition, setDotPosition] = useState('top');
+    const handlePositionChange = ({ target: { value } }) => {
+        setDotPosition(value);
+    };
 
     const cardTitleTurno = 'Turnos'
 
@@ -18,6 +30,31 @@ const Agenda = () => {
             <Menu />
             <div className='agenda_field'>
                 <div className="agenda-field_eventos-alarmes">
+                    <Radio.Group
+                        onChange={handlePositionChange}
+                        value={dotPosition}
+                        style={{
+                            marginBottom: 8,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Radio.Button   style={{
+                            background: '#35225F',
+                            color: '#fff'
+                        }} 
+                            value="left">Eventos</Radio.Button>
+                        <Radio.Button value="right">Alarmes</Radio.Button>
+                    </Radio.Group>
+                    <Carousel dotPosition={dotPosition}>
+                        <div>
+                            <h3 style={contentStyle}>1</h3>
+                        </div>
+                        <div>
+                            <h3 style={contentStyle}>2</h3>
+                        </div>
+                    </Carousel>
 
                 </div>
                 <div className="agenda-field_calendario-turnos">
@@ -29,9 +66,8 @@ const Agenda = () => {
                             <h3 className="calendario-turno_turnos-titulo">{cardTitleTurno}</h3>
                             <div className="agenda-card-turno_field">
 
-                            <CardTurno />
-                            <CardTurno />
-                            <CardTurno />
+                                <CardTurno />
+                                <CardTurno />
                             </div>
                         </div>
 
@@ -41,5 +77,6 @@ const Agenda = () => {
         </div>
     );
 }
+
 
 export default Agenda;
